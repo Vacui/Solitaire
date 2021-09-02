65b3cb6e46e37dd3dc8ed6b48c595b55fd84eb4f
vacui <graizzaromatteo@gmail.com>
2021-09-02 22:40:07 +0200

    style: add game version to menu UI

--- Assets/_Base/Scripts/UI/GetGameVersion.cs
+++ Assets/_Base/Scripts/UI/GetGameVersion.cs
@@ -5,6 +5,10 @@ namespace UI {
     [RequireComponent(typeof(RectTransform)), DisallowMultipleComponent, RequireComponent(typeof(TextMeshProUGUI))]
     public class GetGameVersion : MonoBehaviour {
 
+        private void Awake() {
+            UpdateText();
+        }
+
         /// <summary>
         /// Update the attached text with the game version.
         /// </summary>
